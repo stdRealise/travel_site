2084824320ecbf516e70513f518a4a76ac51855b
stdRealise <telebashnia@gmail.com>
2025-06-19 08:55:19 +0500

    add indexController

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\IndexController;
 
-Route::get('/', function () {
-    return view('pages.index');
-});
+Route::get('/', [IndexController::class, 'index']);
